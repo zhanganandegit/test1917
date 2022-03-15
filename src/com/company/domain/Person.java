26080e1bd0a3d9b534c618308f611e2547fdcd78
zs <zs@bjpowernode.com>
2022-03-15 14:32:26 +0800

    添加了id

---
+++ src/com/company/domain/Person.java
@@ -0,0 +1,15 @@
+package com.company.domain;
+
+/**
+ * ClassName: Person
+ * Package: com.company.domain
+ * Description:
+ *
+ * @date: 2022/3/15 11:35
+ * @author: Anan
+ */
+public class Person {
+
+    private String id;
+
+}
